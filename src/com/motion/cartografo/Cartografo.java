426e3ee37b67a5302292d258e130586222df288d
gr0x0 <dmjpaiva@gmail.com>
2015-06-14 00:20:27 -0300

    Commit#13
    - Adicionando as linhas de código relativas à validação do arquivo XML
    utilizando um arquivo XSD (XML Schema) à Cartografo.

--- src/com/motion/cartografo/Cartografo.java
+++ src/com/motion/cartografo/Cartografo.java
@@ -62,6 +62,7 @@
 
 package com.motion.cartografo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 ///*INTERFACE CODE*/import java.util.Iterator;
@@ -70,6 +71,7 @@ import java.util.ListIterator;
 import com.motion.navegador.Navegador;
 
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
 
 import com.motion.supportClasses.XMLParser;
 import com.motion.supportClasses.ObservadorIF;
@@ -82,52 +84,59 @@ public class Cartografo implements ObservadorIF, ObservadoIF {
 	protected 	static MarList listaMares = new MarList();
 	private 	static Mar marAtual = null;	
 	private 	ArrayList<ObservadorIF> listaObservadores = new ArrayList<ObservadorIF>();
-	
-	final String xmlZonesPath = "resources\\zonas.xml";
+
+	final String xmlZonesPath 		= "resources\\zonas.xml";
+	final String schemaZonesPath	= "resources\\schema.xsd";
 
 	private double 	latMinima = 1000, lonMinima = 1000, latMaxima = -1000, lonMaxima = -1000;
 
 	//----------CONSTRUTOR----------//
 	private Cartografo(){
 
-		// Aqui entra a rotina de try catch pra pegar os dados das zonas relativas ao ônibus.
-		// Atualmente e com intenção de teste, tais dados estão sendo pegos de um arquivo texto chamado
-		// "Zones_Data_Test.txt", contido no diretório 'resources'.
-
 		//Abrindo arquivo texto de Mares
-		XMLParser parser = new XMLParser(xmlZonesPath);
-		List<org.jdom2.Element> elementsList = parser.openArchive();
+		XMLParser parser;
+		try {
+			parser = new XMLParser(xmlZonesPath, schemaZonesPath);
+			List<org.jdom2.Element> elementsList = parser.openArchive();
 
 			/*TEST PRINT*/System.out.print("------Início da leitura de zonas e alocação de Mares------\n");
 
-		//Instanciando todos os Mares contidos no arquivo xml
-		for (org.jdom2.Element zoneElement : elementsList) {
-			String id = zoneElement.getAttributeValue("id");
-			double lat0 = Double.parseDouble(zoneElement.getChildText("lat0"));
-			double latF = Double.parseDouble(zoneElement.getChildText("latF"));
-			double lon0 = Double.parseDouble(zoneElement.getChildText("lon0"));
-			double lonF = Double.parseDouble(zoneElement.getChildText("lonF"));
+			//Instanciando todos os Mares contidos no arquivo xml
+			for (org.jdom2.Element zoneElement : elementsList) {
+				String id = zoneElement.getAttributeValue("id");
+				double lat0 = Double.parseDouble(zoneElement.getChildText("lat0"));
+				double latF = Double.parseDouble(zoneElement.getChildText("latF"));
+				double lon0 = Double.parseDouble(zoneElement.getChildText("lon0"));
+				double lonF = Double.parseDouble(zoneElement.getChildText("lonF"));
 
-			List<Element> impressoesElements = zoneElement.getChild("impressoes").getChildren();
-			ArrayList<String> listaImpressoes = new ArrayList<String>();
-			for(int i = 0; impressoesElements.get(i)!=null; i++)
-			{
-				listaImpressoes.add(impressoesElements.get(i).getText());
-				/*TEST PRINT*/System.out.print("Variaveis "+i+" = "+"id "+id+"| lon0 "+lon0+"| lonF "+lonF+"| lat0 "+lat0+"| latF "+latF+"\n");
+				List<Element> impressoesElements = zoneElement.getChild("impressoes").getChildren();
+				ArrayList<String> listaImpressoes = new ArrayList<String>();
+				for(int i = 0; impressoesElements.get(i)!=null; i++)
+				{
+					listaImpressoes.add(impressoesElements.get(i).getText());
+					/*TEST PRINT*/System.out.print("Variaveis "+i+" = "+"id "+id+"| lon0 "+lon0+"| lonF "+lonF+"| lat0 "+lat0+"| latF "+latF+"\n");
+				}
+
+				Mar novoMar = new Mar(id, lat0, latF, lon0, lonF, listaImpressoes);
+				Cartografo.listaMares.add(novoMar);
+
+				//Guardando as lat e lon mínimas e máximas
+				if(this.latMinima>lat0)
+					latMinima = lat0;
+				if(this.lonMinima>lon0)
+					lonMinima = lon0;
+				if(this.latMaxima<latF)
+					latMaxima = latF;
+				if(this.lonMaxima<lonF)
+					lonMaxima = lonF;
 			}
 
-			Mar novoMar = new Mar(id, lat0, latF, lon0, lonF, listaImpressoes);
-			Cartografo.listaMares.add(novoMar);
-
-			//Guardando as lat e lon mínimas e máximas
-			if(this.latMinima>lat0)
-				latMinima = lat0;
-			if(this.lonMinima>lon0)
-				lonMinima = lon0;
-			if(this.latMaxima<latF)
-				latMaxima = latF;
-			if(this.lonMaxima<lonF)
-				lonMaxima = lonF;
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		//Inscrevendo-se como observador junto ao módulo Navegador
@@ -146,23 +155,23 @@ public class Cartografo implements ObservadorIF, ObservadoIF {
 		}		
 	}
 
-/*INTERFACE CODE BEGIN*/
-//	public int[][] getMaresCoordinates() 
-//	{	
-//		int[][] mares = new int[Cartografo.listaMares.size()][4];
-//		Mar marAux;
-//		Iterator<Mar> iterator = Cartografo.listaMares.iterator();
-//		for(int i = 0; iterator.hasNext(); i++)
-//		{
-//			marAux = iterator.next();
-//			mares[i][0] = (int)marAux.getLonFim();
-//			mares[i][1] = (int)marAux.getLatFim();
-//			mares[i][2] = (int)marAux.getLonIni();
-//			mares[i][3] = (int)marAux.getLatIni();
-//		}
-//		return mares;
-//	}
-/*INTERFACE CODE END*/
+	/*INTERFACE CODE BEGIN*/
+	//	public int[][] getMaresCoordinates() 
+	//	{	
+	//		int[][] mares = new int[Cartografo.listaMares.size()][4];
+	//		Mar marAux;
+	//		Iterator<Mar> iterator = Cartografo.listaMares.iterator();
+	//		for(int i = 0; iterator.hasNext(); i++)
+	//		{
+	//			marAux = iterator.next();
+	//			mares[i][0] = (int)marAux.getLonFim();
+	//			mares[i][1] = (int)marAux.getLatFim();
+	//			mares[i][2] = (int)marAux.getLonIni();
+	//			mares[i][3] = (int)marAux.getLatIni();
+	//		}
+	//		return mares;
+	//	}
+	/*INTERFACE CODE END*/
 
 	public int[] getMaresMinimumLimits()
 	{
@@ -178,9 +187,9 @@ public class Cartografo implements ObservadorIF, ObservadoIF {
 		double longitude = getList[1];
 
 		marAtual = Cartografo.listaMares.findMar(latitude, longitude);
-//		/*TEST CODE*/if(marAtual!=null)
-//			/*TEST PRINT*/System.out.print("marAtual = "+marAtual.getId()+"\n");
-//		/*TEST CODE*/else System.out.print("marAtual = "+marAtual+"\n");
+		//		/*TEST CODE*/if(marAtual!=null)
+		//			/*TEST PRINT*/System.out.print("marAtual = "+marAtual.getId()+"\n");
+		//		/*TEST CODE*/else System.out.print("marAtual = "+marAtual+"\n");
 	}
 
 	@Override
